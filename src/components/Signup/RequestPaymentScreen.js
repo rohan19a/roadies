f5d7f305de45d7f491e330ba06f97a2f249c527c
rohan19a <arohan19a2@gmail.com>
2023-05-20 23:36:08 -0700

    req

--- src/components/Signup/RequestPaymentScreen.js
+++ src/components/Signup/RequestPaymentScreen.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
-import styles from '../styleSheets/styles';
-styles;
+import request_payment_styles from '../styleSheets/request_payment_styles';
 
 const RequestPaymentScreen = () => {
   const [cardNumber, setCardNumber] = useState('');
@@ -26,37 +25,37 @@ const RequestPaymentScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Request Payment</Text>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Card Number:</Text>
+    <View style={request_payment_styles.container}>
+      <Text style={request_payment_styles.title}>Request Payment</Text>
+      <View style={request_payment_styles.inputContainer}>
+        <Text style={request_payment_styles.label}>Card Number:</Text>
         <TextInput
-          style={styles.input}
+          style={request_payment_styles.input}
           value={cardNumber}
           onChangeText={handleCardNumberChange}
           placeholder="Enter card number"
         />
       </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Expiration Date:</Text>
+      <View style={request_payment_styles.inputContainer}>
+        <Text style={request_payment_styles.label}>Expiration Date:</Text>
         <TextInput
-          style={styles.input}
+          style={request_payment_styles.input}
           value={expirationDate}
           onChangeText={handleExpirationDateChange}
           placeholder="MM/YY"
         />
       </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>CVV:</Text>
+      <View style={request_payment_styles.inputContainer}>
+        <Text style={request_payment_styles.label}>CVV:</Text>
         <TextInput
-          style={styles.input}
+          style={request_payment_styles.input}
           value={cvv}
           onChangeText={handleCVVChange}
           placeholder="Enter CVV"
         />
       </View>
-      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-        <Text style={styles.buttonText}>Submit Payment</Text>
+      <TouchableOpacity style={request_payment_styles.button} onPress={handleSubmit}>
+        <Text style={request_payment_styles.buttonText}>Submit Payment</Text>
       </TouchableOpacity>
     </View>
   );
---
+++ src/components/styleSheets/request_payment_styles.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+
+const request_payment_styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  inputContainer: {
+    marginBottom: 15,
+    width: '100%',
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#f4511e',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default request_payment_styles;
